83e7a32a1be4764d7b5a41e85effe2df11f9486b
Brick Lai <l279810259@gmail.com>
2022-11-22 18:03:11 -0600

    initial commit

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,64 +1,53 @@
 'use strict';
 
-const start = new Audio('./assets/audio/alarm.mp3');
-start.type = 'audio/mp3';
+let createbtn = document.querySelector('.createbtn');
+let shapebtn = document.querySelector('.shapebtn');
+let colorbtn = document.querySelector('.colorbtn');
+let mainbox = document.querySelector('.mainbox');
+let shape = document.querySelectorAll('.shape-content ul li');
+let color = document.querySelectorAll('.color-content ul li');
+let items = document.querySelectorAll('.item');
+for (let i = 0; i < shape.length; i++) {
+    shape[i].addEventListener('click', function() {
+        shapebtn.innerHTML = shape[i].innerHTML;
+    });
+};
 
-time();
 
-function time(){
-    var time = new Date();
-    var hour;
-    var minutes;
-    var seconds;
-    var temph = time.getHours();
-    if(temph <=9 && temph >0){
-        hour = "0"+ temph;
-    }else{
-        hour=temph;
-    }
-    var tempm=time.getMinutes();
-    if(tempm <10 && tempm >=0){
-        minutes="0"+tempm;
-    }else{
-        minutes=tempm;
-    }
-    var temps = time.getSeconds();
-    if(temps < 10 && temps >=0){
-        seconds= "0"+temps;
-    }else{
-        seconds =temps;
-    }
+for(let i = 0; i < color.length; i++) {
+    color[i].addEventListener('click', function() {
+        colorbtn.innerHTML = color[i].innerHTML;  
+    });
+};
 
-    document.getElementById("time-hour").innerHTML = hour;
-    document.getElementById("time-minutes").innerHTML = minutes;
-    document.getElementById("time-second").innerHTML = seconds;
-}
-    window.setInterval("time()",1000);
+let arr = [];
+for(let i = 0; i < items.length; i++) {
+    arr.push(items[i]);
+};
 
-let input1 = document.querySelector('.timeinput1');
-let input2 = document.querySelector('.timeinput2');
-let showTime = document.querySelector('.timedistance');
-let btn = document.querySelector('.setalarm');
-let change = document.querySelector('.currenttime');
+console.log(arr);
 
-btn.addEventListener('click', function() {
-    let h = input1.value;
-    let m = input2.value;
-    showTime.innerHTML = `${h} : ${m}`;
-    console.log(h);
-    console.log(m);
+let i = 23;
+createbtn.addEventListener('click', function() {
+    let shape = shapebtn.innerHTML;
+    let color = colorbtn.innerHTML;
     
-    setInterval(function() {
-        var time = new Date();
-        var temph = time.getHours().toString().padStart(2, '0');
-        var tempm = time.getMinutes().toString().padStart(2, '0');
-        if(h === temph && m === tempm) {
-            change.style.color = '#4dc2df';
-            start.play();
-          };
-    },1000)
+    if(shape == 'Circle') {
+        arr[i].style.borderRadius = '50%';
+    } else {
+        arr[i].style.borderRadius = ' ';
+    };
 
-
-});
-
-
+    if(color == 'Green') {
+        arr[i].style.backgroundColor = '#9f0';
+    } else if(color == 'Purple') {
+        arr[i].style.backgroundColor = '#90f';
+    } else if(color == 'Pink') {
+        arr[i].style.backgroundColor = '#f09';
+    } else if(color == 'Blue') {
+        arr[i].style.backgroundColor = '#0f9';
+    } else {
+        arr[i].style.backgroundColor = '#f90';
+    };
+    i = i - 1;
+})
