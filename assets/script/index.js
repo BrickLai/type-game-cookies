2d5d2237e69e316dd3373cd23a0069640321baf6
Brick Lai <l279810259@gmail.com>
2022-11-25 22:44:00 -0600

    add

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,53 +1,59 @@
 'use strict';
 
-let createbtn = document.querySelector('.createbtn');
-let shapebtn = document.querySelector('.shapebtn');
-let colorbtn = document.querySelector('.colorbtn');
-let mainbox = document.querySelector('.mainbox');
-let shape = document.querySelectorAll('.shape-content ul li');
-let color = document.querySelectorAll('.color-content ul li');
-let items = document.querySelectorAll('.item');
-for (let i = 0; i < shape.length; i++) {
-    shape[i].addEventListener('click', function() {
-        shapebtn.innerHTML = shape[i].innerHTML;
-    });
-};
-
-
-for(let i = 0; i < color.length; i++) {
-    color[i].addEventListener('click', function() {
-        colorbtn.innerHTML = color[i].innerHTML;  
-    });
-};
-
+let addBtn = document.querySelector('.addbtn');
+let contacts = document.querySelector('.contactsbox');
+let form = document.querySelector(".form");
 let arr = [];
-for(let i = 0; i < items.length; i++) {
-    arr.push(items[i]);
+let arr2 = [];
+
+const emailRegex = /^(?=^.{8,}$)[-_A-Za-z0-9]+([_.-][a-zA-Z0-9]+)*@[A-Za-z0-9]+([.-][a-zA-Z0-9]+)*\.[A-Za-z]{2,}$/;
+addBtn.addEventListener('click', function() {
+    let inputName = document.querySelector(".inputname").value.trim() ;
+    let inputCity = document.querySelector(".inputcity").value.trim() ;
+    let inputEmail = document.querySelector(".inputemail").value.trim();
+    let contact = document.createElement('div');
+    arr.push(inputName);
+    arr.push(inputCity);
+    arr.push(inputEmail);
+    
+
+    if(inputName.length === 0) {
+        alert('Name is required');
+    } else if (inputCity.length === 0) {
+        alert('City is required');
+    } else if (inputEmail.length === 0) {
+        alert('Email is  required')
+    } else if (!emailRegex.test(inputEmail)) {
+        alert('A valid email is  required')
+    } else {
+        
+      for(let i = 0; i < arr.length; i++) {
+       
+        contact.innerHTML = `Name:${arr[i - 2]}<br>City:${arr[i - 1]}<br>Email:${arr[i]}`;
+        contacts.insertBefore(contact, contacts.children[0]);
+
+
+        
+       } 
+       
+    }
+
+})
+
+
+
+/* let arr = [];
+for(let i = 0; i < contacts.length; i++) {
+    arr.push(contacts[i]);
 };
 
 console.log(arr);
 
-let i = 23;
-createbtn.addEventListener('click', function() {
-    let shape = shapebtn.innerHTML;
-    let color = colorbtn.innerHTML;
+let i = 1;
+addBtn.addEventListener('click', function() {
+    let inputName = inputName.innerHTML;
+    let inputCity = inputCity.innerHTML;
+    let inputEmail = inputEmail.innerHTML;
     
-    if(shape == 'Circle') {
-        arr[i].style.borderRadius = '50%';
-    } else {
-        arr[i].style.borderRadius = ' ';
-    };
-
-    if(color == 'Green') {
-        arr[i].style.backgroundColor = '#9f0';
-    } else if(color == 'Purple') {
-        arr[i].style.backgroundColor = '#90f';
-    } else if(color == 'Pink') {
-        arr[i].style.backgroundColor = '#f09';
-    } else if(color == 'Blue') {
-        arr[i].style.backgroundColor = '#0f9';
-    } else {
-        arr[i].style.backgroundColor = '#f90';
-    };
-    i = i - 1;
 })
+ */
